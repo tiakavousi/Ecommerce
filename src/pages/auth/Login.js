f9f72c9682d43a3b9d10471099ac6a9df8bdaaf4
Tayebe kavousi <tayebekavousi68@gmail.com>
2022-12-27 22:54:56 +0800

    creating react app and creating pages : login, register and home , adding bootstrap to the index.html

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+    return(
+        <div>
+            <h1> login </h1>
+            <p> Login </p>
+        </div> 
+    );
+};
+  
+export default Login;
---
+++ src/pages/auth/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+    return(
+        <div >
+            <h1> register </h1>
+            <p> Register </p>
+        </div>
+    );
+};
+ 
+export default Register;
+  
