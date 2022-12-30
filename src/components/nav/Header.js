a52cbb146c0024ebdcbb1559fb5ba0e5d140edd5
Tayebe kavousi <tayebekavousi68@gmail.com>
2022-12-30 15:28:22 +0800

    changing header

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,24 +1,46 @@
-import React, {useState} from "react";
-import {Menu} from "antd";
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+    HomeOutlined, 
+    UserOutlined, 
+    UserAddOutlined,
+    ProfileFilled 
+} from "@ant-design/icons";
+import {Link} from "react-router-dom";
 
-const{SubMenu} = Menu;
+const{SubMenu, Item} = Menu;
 
 const Header = () => {
+
     const [current, setCurrent]= useState('');
-    const handleClick = () =>{
-        //
+
+    const handleClick = (e) => {
+        console.log(e.key);
+        setCurrent(e.key);
     }
+
     return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Menu.Item key="mail">
-            Home
-            </Menu.Item>
-            <SubMenu title="Navigation Three - Submenu">
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" >
+
+            <Item key="home" icon={<HomeOutlined />}>
+                <Link to="/"> Home </Link> 
+            </Item>
+
+            <SubMenu title="Profile" key="profile" icon={<ProfileFilled />}> {/* title in the video = "username"*/}
                 <Menu.ItemGroup title="Item 1">
-                    <Menu.Item key="setting:1">Option1</Menu.Item>
-                    <Menu.Item key="setting:2">Option2</Menu.Item>
+                    <Item key="setting:1">Option1</Item>
+                    <Item key="setting:2">Option2</Item>
                 </Menu.ItemGroup>
-            </SubMenu>
+            </SubMenu> 
+
+            <Item key="register" icon={<UserAddOutlined />} className="float-right"> {/*float-right classname not working*/}
+                <Link to="/register"> Register </Link> 
+            </Item>
+
+            <Item key="login" icon={<UserOutlined />} className="float-right"> {/*float-right classname not working*/}
+                <Link to="/login"> Login </Link>
+            </Item>
+
         </Menu>
     );
 }
